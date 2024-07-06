78bcb40317b5b402ac39a499aa1671c55ad7c391
ShirinHasan13 <shirin10@chicagobooth.edu>
2024-07-06 18:04:58 +0000

    d

---
+++ app2.rb
@@ -0,0 +1,51 @@
+require "sinatra"
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+get("/") do
+  redirect("/add")
+end
+
+get("/add") do
+  erb(:add_form)
+end
+
+get("/wizard_add") do
+  @first_num = params.fetch("first_num").to_f
+
+  @result = @first_num + @second_num
+  erb(:add_result)
+end
+
+get("/subtract") do
+  erb(:sub_form)
+end
+
+get("/wizard_subtract") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  erb(:sub_result)
+end
+
+get("/multiply") do
+  erb(:multiplication_form)
+end
+
+get("/wizard multiply") do
+  erb(:mult_result)
+end
+
+get("/divide") do
+  erb(:div_form)
+end
+
+get("/wizard_divide") do
+  erb(:div_result)
+end
